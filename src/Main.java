41827767e33e3ff6106ddb44a6c7b1e4770cb0b5
linskay <alina.cheremiskina@gmail.com>
2024-05-24 22:25:01 +0300

    complete v.2

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         int start = 0;
         int mounth = 0;
         while (start < 2_459_000) {
-            start = start + start / 100;
+            start = start + (start * 12 / 100 / 12);
             start = deposit + start;
-            mounth = mounth + 1;
+            mounth++;
             System.out.println("Месяц " + mounth + ", сумма накоплений равна " + start + " рублей");
         }
         System.out.println("Task 2");
@@ -23,37 +23,35 @@ public class Main {
         }
         System.out.println();
         System.out.println("Task 3");
-        int countryY = 12_000_000;
-        double death = 0.008;
-        double birthRate = 0.017;
+        int countryYPopulation = 12_000_000;
+        double deathPercent = 0.008;
+        double birthRatePercent = 0.017;
         for (int i = 1; i <= 10; i++) {
-            int population = (int) ((birthRate - death) * countryY);
-            countryY += population;
-            System.out.println("Год " + i + ", численность население составляет " + countryY);
+            int population = (int) ((birthRatePercent - deathPercent) * countryYPopulation);
+            countryYPopulation += population;
+            System.out.println("Год " + i + ", численность население составляет " + countryYPopulation);
         }
         System.out.println("Task 4");
         int depositVasiliy = 15000;
         int totalVasiliy = 0;
         for (int mounthVasya = 1; totalVasiliy < 12_000_000; mounthVasya++) {
-            totalVasiliy = (int) (totalVasiliy + totalVasiliy * 0.07);
+            totalVasiliy += totalVasiliy * 0.07;
             totalVasiliy = depositVasiliy + totalVasiliy;
             System.out.println("Месяц " + mounthVasya + ", накопления = " + totalVasiliy);
         }
         System.out.println("Task 5");
-        int depositVasiliyTask5 = 15000;
         int totalVasiliyTask5 = 0;
         for (int mounthVasyaTask5 = 1; totalVasiliyTask5 < 12_000_000; mounthVasyaTask5++) {
-            totalVasiliyTask5 = (int) (totalVasiliyTask5 + totalVasiliyTask5 * 0.07);
-            totalVasiliyTask5 = depositVasiliyTask5 + totalVasiliyTask5;
+            totalVasiliyTask5 += totalVasiliyTask5 * 0.07;
+            totalVasiliyTask5 = depositVasiliy + totalVasiliyTask5;
             if (mounthVasyaTask5 % 6 == 0)
                 System.out.println("Месяц " + mounthVasyaTask5 + ", накопления = " + totalVasiliyTask5);
         }
         System.out.println("Task 6");
-        int depositVasiliyTask6 = 15000;
         int totalVasiliyTask6 = 0;
         for (int mounthVasyaTask6 = 1; mounthVasyaTask6 <= 9 * 12; mounthVasyaTask6++) {
-            totalVasiliyTask6 = (int) (totalVasiliyTask6 + totalVasiliyTask6 * 0.07);
-            totalVasiliyTask6 = depositVasiliyTask6 + totalVasiliyTask6;
+            totalVasiliyTask6 += totalVasiliyTask6 * 0.07;
+            totalVasiliyTask6 = depositVasiliy + totalVasiliyTask6;
             if (mounthVasyaTask6 % 6 == 0)
                 System.out.println("Месяц " + mounthVasyaTask6 + ", накопления = " + totalVasiliyTask6);
         }
@@ -61,7 +59,6 @@ public class Main {
         int friday = 7;
         for (int day = 1; day <= 31; day++) {
             if (friday % 7 == day % 7)
-
                 System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет");
         }
         System.out.println("Task 7.2");
